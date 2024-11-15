f889c98ca2ecd1cc1050f87b99e3070bb5cc0342
todoggler <kentokuraCS@gmail.com>
2024-11-15 18:42:48 +0900

    chore: aaa

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -10,7 +10,7 @@ class SampleItemDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Item Details Test'),
+        title: const Text('Item Details Test aaa'),
       ),
       body: const Center(
         child: Text('More Information Here'),
